0b5cd3b505d2e50e079f622f12325ea167e5f74f
akram-gorashi <akramgorashi6@gmail.com>
2020-08-08 13:27:02 +0200

    [update] removes console logs

--- src/app/components/vehicle/vehicle-list/vehicle-list.component.ts
+++ src/app/components/vehicle/vehicle-list/vehicle-list.component.ts
@@ -10,7 +10,9 @@ import { PaginationResponse } from 'src/app/models/app/Vehicle/PaginationRespons
   templateUrl: './vehicle-list.component.html',
   styleUrls: ['./vehicle-list.component.css'],
 })
+
 export class VehicleListComponent implements OnInit {
+   
   @Input() vehicles: Vehicle[];
   assetsUrl: string = environment.assetsUrl;
   paginationInfo: PaginationResponse = <PaginationResponse>{};
@@ -22,8 +24,6 @@ export class VehicleListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    console.log('fghddgd');
-
     // load page based on 'page' query param or default to 1
     this.getCurrentPage();
     //TODO: fix loader
@@ -39,27 +39,20 @@ export class VehicleListComponent implements OnInit {
       this.route.queryParams.subscribe((params) => {
         pageNumber = params['pageNumber'];
       });
-     this.changeParms(pageNumber);
-      console.log(pageNumber);
+      this.changeParms(pageNumber);
       if (pageNumber == undefined) {
-        this.getVehicleList();
       } else {
-        console.log(pageNumber);
         this.changeParms(pageNumber);
-        console.log('getVehicleList(parms)');
         this.getVehicleList({ pageNumber: pageNumber });
       }
     }
   }
   getVehicleList(pageQuery?: Object) {
-    console.log(pageQuery);
     this.vehicleService.requestGetVehicles(pageQuery);
     this.vehicleService.getVehicles().subscribe((vehicles: Vehicle[]) => {
       this.vehicles = vehicles;
       this.paginationInfo = this.vehicleService.pagination.value;
-      console.log(this.paginationInfo);
     });
-    console.log(this.vehicles);
   }
 
   watchLoader() {
@@ -68,28 +61,14 @@ export class VehicleListComponent implements OnInit {
     });
   }
   pageChanged(e: any) {
-    console.log(e.page + 'page changed');
     let pageQuery = { pageNumber: e.page, pageSize: e.itemsPerPage };
-    console.log(pageQuery);
-    /*  this.router.navigate([], {
-      queryParams: {
-        pageNumber: pageQuery.pageNumber,
-      },
-    }); */
-    /* this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: pageQuery,
-    }); */
-    console.log(pageQuery);
     this.getCurrentPage(pageQuery);
-    //  this.vehicleService.requestGetVehicles(pageQuery);
   }
   changeParms(newParams) {
-   this.router.navigate([], {
+    this.router.navigate([], {
       queryParams: {
         pageNumber: newParams,
       },
     });
-    console.log(newParams);
   }
 }
